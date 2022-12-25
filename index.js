d10751083390b3d640d8d4022d150937cd19637a
Eyasin81 <eyasina81@gmail.com>
2022-12-25 15:07:39 +0600

    update

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ if(error){
     console.log("Data Connection Fail")
 }else{
     console.log("Data Connection success")
-    InsertData(MyMongoClint)
+    InsertData(MyMongoClient)
 }
 })
-function InsertData(MyMongoClint){
-    var MyDataBase=MyMongoClint.db("student");
+function InsertData(MyMongoClient){
+    var MyDataBase=MyMongoClient.db("student");
     var MyCollection=MyDataBase.collection('class');
     var MyData={name:"Abdul ALi",Roll:"334756",Reg:"34343"};
-    MyCollection.insertOne(MyData,function(error){
-        if(error){
+    MyCollection.insertOne(MyData,option,function(err){
+        if(err){
             console.log("Data Insert Fail")
         }else{
             console.log("Data Insert Success")
         }
     })
     
-}
-
+};
