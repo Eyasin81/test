465c4cf63256805ecba348460c079aac83c80ecd
Eyasin81 <eyasina81@gmail.com>
2022-12-25 21:02:32 +0600

    add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mongoClint=require('mongodb').MongoClient;
 var url="mongodb+srv://eyasina:c0W2slssudMIm5kd@cluster0.ecuhx2g.mongodb.net/?retryWrites=true&w=majority"
 var config={useUnifiedTopology:true};
-mongoClint.connect(url,config,function(error,MyData){
+mongoClint.connect(url,config,function(error,MyMongoClient){
 if(error){
     console.log("Data Connection Fail")
 }else{
@@ -13,12 +13,13 @@ function InsertData(MyMongoClient){
     var MyDataBase=MyMongoClient.db("student");
     var MyCollection=MyDataBase.collection('class');
     var MyData={name:"Abdul ALi",Roll:"334756",Reg:"34343"};
-    MyCollection.insertOne(MyData,option,function(err){
-        if(err){
+    MyCollection.insertOne(MyData,function(error){
+        if(error){
             console.log("Data Insert Fail")
         }else{
             console.log("Data Insert Success")
         }
     })
     
-};
+}
+
