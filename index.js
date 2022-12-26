b25864718a306466062653fba65ee6582455e371
Eyasin81 <eyasina81@gmail.com>
2022-12-26 13:40:51 +0600

    Data Insert MongoDB

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+// Data Insert MongoDB 
 var mongoClint=require('mongodb').MongoClient;
-var url="mongodb+srv://eyasina:c0W2slssudMIm5kd@cluster0.ecuhx2g.mongodb.net/?retryWrites=true&w=majority"
+var url="mongodb+srv://eyasinaa:mnVqGwYpAGDd88p8@cluster0.ecuhx2g.mongodb.net/?retryWrites=true&w=majority"
 var config={useUnifiedTopology:true};
-mongoClint.connect(url,config,function(error,MyMongoClient){
-if(error){
+mongoClint.connect(url,config,function(err,MyMongoClinet){
+if(err){
     console.log("Data Connection Fail")
 }else{
     console.log("Data Connection success")
-    InsertData(MyMongoClient)
+    InsertData(MyMongoClinet);
 }
 })
-function InsertData(MyMongoClient){
-    var MyDataBase=MyMongoClient.db("student");
-    var MyCollection=MyDataBase.collection('class');
-    var MyData={name:"Abdul ALi",Roll:"334756",Reg:"34343"};
-    MyCollection.insertOne(MyData,function(error){
-        if(error){
+function InsertData(MyMongoClinet){ 
+    const MyDataBase=MyMongoClinet.db("school");
+    const MyCollection=MyDataBase.collection('students');
+    const MyData={name:"Abdullah",Roll:"334736",Reg:"265836"};
+    MyCollection.insertOne(MyData,function(err){
+        if(err){
             console.log("Data Insert Fail")
         }else{
             console.log("Data Insert Success")
