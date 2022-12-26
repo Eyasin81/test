34a53e8b833cc9e9feb6c6c374b21c9c8166e1c8
Eyasin81 <eyasina81@gmail.com>
2022-12-26 07:42:20 +0600

    comment

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ if(error){
 }else{
     console.log("Data Connection success")
     InsertData(MyMongoClient)
+
 }
 })
-function InsertData(MyMongoClient){
-    var MyDataBase=MyMongoClient.db("student");
-    var MyCollection=MyDataBase.collection('class');
-    var MyData={name:"Abdul ALi",Roll:"334756",Reg:"34343"};
-    MyCollection.insertOne(MyData,function(error){
-        if(error){
-            console.log("Data Insert Fail")
-        }else{
-            console.log("Data Insert Success")
-        }
-    })
+    //problem
+// function InsertData(MyMongoClient){
+//     var MyDataBase=MyMongoClient.db("student");
+//     var MyCollection=MyDataBase.collection('class');
+//     var MyData={name:"Abdul ALi",Roll:"334756",Reg:"34343",};
+//     MyCollection.insertOne(MyData,function(error){
+//         if(error){
+//             console.log("Data Insert Fail")
+//         }else{
+//             console.log("Data Insert Success")
+//         }
+//     })
     
-}
+// }
 
