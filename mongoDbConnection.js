3f0987448e8e30be8840c50ee8a953f808ab6e85
Eyasin81 <eyasina81@gmail.com>
2022-12-27 12:35:24 +0600

    update

--- mongoDbConnection.js
+++ mongoDbConnection.js
@@ -1,4 +1,4 @@
-// MongoDb Connection
+// MongoDb Connection 
 var mongoClint=require('mongodb').MongoClient;
 var url="mongodb+srv://<User Name>:< password>.ecuhx2g.mongodb.net/?retryWrites=true&w=majority"
 var config={useUnifiedTopology:true};
